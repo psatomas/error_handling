0ae3468701ba9468779cf2bd143bd938f96636ef
psatomas <psatomas@gmail.com>
2025-08-28 12:55:50 -0300

    refactor: return file contents directly from read_file

---
+++ src/bin/the_read_to_string_associated_function.rs
@@ -0,0 +1,24 @@
+use std::fs;
+use std::io::{self, stdin};
+
+
+
+fn main() {
+    let file_result = read_file();
+
+    match file_result {
+        Ok(contents) => println!("{contents}"),
+        Err(error) => {
+            eprintln!("There was an error: {error:?}");
+        }
+    }
+}
+
+fn read_file() -> Result<String, io::Error> {
+    println!("Please enter the name of the file you'd like to read:");
+
+    let mut input: String = String::new();
+    stdin().read_line(&mut input)?;
+
+    fs::read_to_string(input.trim())
+}
